85dcd1bf2c3ef95bbf166bda3b24c489d56607e6
Arth Raval <connect.arth.raval@gmail.com>
2022-06-09 12:14:57 +0530

    Route for product

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\ProductsController;
+use App\Models\Products;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -20,8 +23,31 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::resource('home', ProductsController::class)->middleware('auth');
+Route::post('product_update/{id}', function ($id, Request $request) {
+    try {
+        // get product by id
+
+        $image = $request->file('image');
+        $imageName = $request->product_upc . '.' . $image->getClientOriginalExtension();
+        $image->move(public_path('images'), $imageName);
+        // get product by id
+        $product = Products::find($id);
+        // dd($product);
+
+        // Update product
+        //get csrf token
+        $product->name = $request->product_name;
+        $product->price = $request->product_price;
+        $product->image = 'images/' .  $imageName;
+        $product->status = $request->product_status == 'Active' ? 1 : 0;
+        $product->save();
+
+        return response()->json(['products' => $product]);
+    } catch (\Exception $e) {
+        //return error message with error line
+        return response()->json($e->getMessage() . ' in ' . $e->getLine(), 400);
+    }
+});
 
 Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
